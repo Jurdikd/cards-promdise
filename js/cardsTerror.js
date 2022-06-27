c1cd50e76c8edec925d2f1bbde4285887aa565d0
Jurdikd <jesuscovo@gmail.com>
2022-06-27 10:52:50 -0400

    Configuaración js

---
+++ js/cardsTerror.js
@@ -0,0 +1,9 @@
+/* Cards resumen*/
+/* Carda del documento*/
+window.addEventListener("load", () => {
+	card_description.forEach((description) => {
+		console.log(description);
+	});
+});
+/* Select every class from card-description*/
+const card_description = document.querySelectorAll("card-description");
